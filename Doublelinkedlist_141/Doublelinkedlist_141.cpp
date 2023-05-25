2171b2df7841df29099ce903b8b0bd356722ccd3
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-05-25 19:45:23 +0700

    Membuat hapus

--- Doublelinkedlist_141/Doublelinkedlist_141.cpp
+++ Doublelinkedlist_141/Doublelinkedlist_141.cpp
@@ -107,9 +107,39 @@ bool DoubleLinkedList::listEmpty() {
     return (START == NULL);
 }
 
+void DoubleLinkedList::traverse() {
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
 
 
+void DoubleLinkedList::revtraverse() {
+    if (listEmpty()) {
+        cout << "\nList is Empty" << endl;
+    }
 
+    else {
+        cout << "\nRecords in descending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
+void DoubleLinkedList::hapus()
 
 
 
